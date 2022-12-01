6099c23fcbf01898e5b58d5d4550ef4168aa8e7c
AntonAcorn <ShustikovAnimation@gmail.com>
2022-12-01 17:38:00 +0300

    Initial commit

---
+++ src/main/java/org/example/SensorRestClient.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class SensorRestClient
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
